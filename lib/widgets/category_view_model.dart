40aafe75a3f3d709045d6c710c0a6dbfd5545ee9
milad98333 <Mosahosseini234@gmail.com>
2023-06-07 16:12:48 +0430

    web enabled for this project

--- lib/widgets/category_view_model.dart
+++ lib/widgets/category_view_model.dart
@@ -6,52 +6,59 @@ class CategoryViewModel extends StatelessWidget {
   final List<String> categoryList;
   final String headerLabel;
   final String assetImage;
+  final String callPleace;
   const CategoryViewModel({
     super.key,
     required this.categoryList,
     required this.headerLabel,
     required this.assetImage,
+    required this.callPleace,
   });
   @override
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Padding(
-        padding: const EdgeInsets.all(4.0),
+        padding: const EdgeInsets.symmetric(horizontal: 4),
         child: Row(
           children: [
             Expanded(
-              child: SizedBox(
-                child: Stack(children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CategHeaderLebel(
-                        headerLebel: headerLabel,
+              child: Stack(children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    callPleace == 'category'
+                        ? CategHeaderLebel(
+                            headerLebel: headerLabel,
+                          )
+                        : const SizedBox(
+                            height: 4,
+                          ),
+                    Expanded(
+                      child: GridView.count(
+                        physics: const BouncingScrollPhysics(),
+                        mainAxisSpacing: 30,
+                        crossAxisCount: 3,
+                        children: List.generate(categoryList.length, (index) {
+                          return SubCategModel(
+                            callPleace: callPleace,
+                            maincategName: headerLabel,
+                            subcategName: categoryList[index],
+                            assteImage: 'assets/images/$assetImage$index.jpg',
+                            subcategLebel: categoryList[index],
+                          );
+                        }),
                       ),
-                      Expanded(
-                        child: GridView.count(
-                          physics: const BouncingScrollPhysics(),
-                          mainAxisSpacing: 30,
-                          crossAxisCount: 3,
-                          children: List.generate(categoryList.length, (index) {
-                            return SubCategModel(
-                              maincategName: headerLabel,
-                              subcategName: categoryList[index],
-                              assteImage: 'assets/images/$assetImage$index.jpg',
-                              subcategLebel: categoryList[index],
-                            );
-                          }),
-                        ),
-                      ),
-                    ],
-                  ),
-                ]),
-              ),
+                    ),
+                  ],
+                ),
+              ]),
             ),
-            SliderBar(
-              maincategName: headerLabel.toUpperCase(),
-            )
+            callPleace == 'category'
+                ? SliderBar(
+                    maincategName: headerLabel,
+                  )
+                : const SizedBox(),
           ],
         ),
       ),
